3b3d7f57d0e6a5ea57b47984b893febbc9ca297b
E-JorgeSalazar <165840533+E-JorgeSalazar@users.noreply.github.com>
2024-07-05 14:18:48 -0600

    xxx
    xxx

---
+++ compenza.authentication.application/Querys/ValidateLicenseBeforeUploading.cs
@@ -0,0 +1,134 @@
+﻿using MediatR;
+using compenza.authentication.domain.Enums;
+using compenza.authentication.domain.Configure;
+using compenza.authentication.percistance.Interfaces;
+using compenza.authentication.application.Utilities;
+using Microsoft.AspNetCore.Http;
+
+namespace compenza.authentication.application.Querys
+{
+    public class ValidateLicenseBeforeUploading
+    {
+        public record Query(HttpContext Context) : IRequest<Result>;
+
+        public class Handler : IRequestHandler<Query, Result>
+        {
+            private readonly ILoginRepository _loginRepository;
+
+            public Handler(ILoginRepository loginRepository)
+            {
+                _loginRepository = loginRepository;
+            }
+
+            public async Task<Result> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var (Empleados, usuarios) = await _loginRepository.UsuarioEmpleadoActivos();
+                var (EmpleadosTotal, UsuariosTotal) = await _loginRepository.UsuarioEmpleadoActivosTotal();
+
+                PropertiesConfig propertiesConfig = new PropertiesConfig();
+
+                Parameters LicenseResult = await propertiesConfig.Information();
+
+                string strPath = request.Context.Request.Host.Value + "/";
+                string ServerIDConfiguration = string.Empty;
+
+                if (!string.IsNullOrEmpty(strPath))
+                {
+                    ServerIDConfiguration = Encripta.EncriptarCadena(strPath);
+                }
+
+                var dtName = await _loginRepository.BaseDeDatosId();
+
+                var CPU = dtName.Rows[0]["ComputerName"];
+                var server = dtName.Rows[0]["InstanceName"];
+                var version = dtName.Rows[0]["ProductVersion"];
+
+                var BDServerIDConfiguration = Encripta.EncriptarCadena(String.Format("{0}\\{1}\\{2}", CPU, server, version));
+
+                int validarServer = await propertiesConfig.ValidarServer(BDServerIDConfiguration, ServerIDConfiguration);
+                var isVlidResult = await ValidarLicencia(LicenseResult, Empleados, usuarios, EmpleadosTotal, UsuariosTotal, validarServer);
+
+                return isVlidResult;
+            }
+
+            public async Task<Result> ValidarLicencia(Parameters license, int empleados, int usuarios, int empleadosTotal, int usuarioTotal, int validarServer)
+            {
+                PropertiesConfig _propertiesConfig = new PropertiesConfig();
+
+                var result = new Result();
+                var diasTolerancia = license.FechaLicencia.Subtract(DateTime.Now).Days;
+                var diasExpiracion = DateTime.Now.Subtract(license.FechaLicencia).Days;
+
+                if (license is null)
+                {
+                    result.Mensaje = "msgLicenciaInvalida";
+                    result.Objeto = (int)eResultado.ErrorLicencia;
+                    result.Res = false;
+
+                    return result;
+                }
+                else if (validarServer == (int)eTipoErrors.ErrorArchivo)
+                {
+                    result.Mensaje = "msgServidorInvalido";
+                    result.Objeto = (int)eTipoErrors.ErrorArchivo;
+                    result.DBServerMessage = $"ID: {license.BDServerID}";
+                    result.ServerMessage = $"ID: {license.ServerID}";
+                    result.Res = false;
+
+
+                    return result;
+                }
+                else if (diasTolerancia <= license.Alerta && diasTolerancia is not < 0)
+                {
+                    result.Mensaje = "msgAlertaLicencia";
+                    result.Objeto = diasTolerancia;
+                    result.Res = true;
+
+                    return result;
+                }
+                else if (DateTime.Now >= license.FechaLicencia && diasExpiracion <= license.Expira)
+                {
+                    result.Mensaje = "msgAlertaLicenciaCaducado";
+                    result.Res = true;
+                }
+                else if (DateTime.Now >= license.FechaLicencia.AddDays(license.Expira))
+                {
+                    result.Mensaje = "msgLicenciaExpirada";
+                    result.Res = false;
+
+                    return result;
+                }
+                else if (empleados > (license.Empleados + (license.Empleados * 0.3)))
+                {
+                    result.Mensaje = "msgLicenciaLimiteEmpleados";
+                    result.Res = false;
+
+                    return result;
+                }
+                else if (usuarios > (license.Usuarios + (license.Usuarios * 0.3)))
+                {
+                    result.Mensaje = "msgLicenciaLimiteUsuarios";
+                    result.Res = false;
+
+                    return result;
+                }
+                else if (await _propertiesConfig.ValidarEmpleados(empleadosTotal) == (int)eTipoErrors.ExeceEmpleados)
+                {
+                    result.Mensaje = "msgLicenciaLimiteEmpleados";
+                    result.Res = false;
+
+                    return result;
+                }
+                else if (await _propertiesConfig.ValidarEmpleados(empleadosTotal) == (int)eTipoErrors.ExeceUsuarios)
+                {
+                    result.Mensaje = "msgLicenciaLimiteUsuarios";
+                    result.Res = false;
+
+                    return result;
+                }
+
+                return result;
+            }
+        }
+    }
+}
---
+++ compenza.authentication.domain/Configure/HttpException.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Runtime.Serialization;
+
+namespace compenza.authentication.domain.Configure
+{
+    [Serializable]
+    internal class HttpException : Exception
+    {
+        private HttpStatusCode locked;
+        private string v;
+        private object errorDetails;
+
+        public HttpException()
+        {
+        }
+
+        public HttpException(string? message) : base(message)
+        {
+        }
+
+        public HttpException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        public HttpException(HttpStatusCode locked, string v, object errorDetails)
+        {
+            this.locked = locked;
+            this.v = v;
+            this.errorDetails = errorDetails;
+        }
+
+        protected HttpException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
